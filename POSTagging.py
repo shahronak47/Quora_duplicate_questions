e486d2cbecd1534bbbe193e939c7cb809991d447
Ronak Shah <shahronak47@yahoo.in>
2017-12-24 15:42:58 +0800

    created a seperate function and seperated subject and object

--- POSTagging.py
+++ POSTagging.py
@@ -1,16 +1,21 @@
 import re
 import spacy
 
-if __name__ == '__main__' :
-    T1 = "RAM keeps things being worked with"
-    T2 = "The CPU uses RAM as a shortterm memory store"
+def get_subject_object (sentence) :
+    #T1 = "RAM keeps things being worked with"
+    #T2 = "The CPU uses RAM as a shortterm memory store"
     #Load the english language
     nlp = spacy.load('en')
-    doc = nlp(T2)
-    subject_object = []
+    doc = nlp(sentence)
+    subject = []
+    object = []
     for tok in doc :
-        #Search for token having subjects or object in them
-        if re.search('subj|obj', tok.dep_) :
+        #Search for token having subj in them
+        if re.search('subj', tok.dep_) :
             #Get the corresponding words
-            subject_object.append(tok.text)
-    print(subject_object)
+            subject.append(tok.text)
+        # Search for token having obj in them
+        if re.search('obj', tok.dep_) :
+            object.append(tok.text)
+
+    return subject, object
