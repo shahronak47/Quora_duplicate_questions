7376e1342ff0117bac6c2f53fb4e981e8857ce63
Ronak Shah <shahronak47@yahoo.in>
2017-12-27 22:25:13 +0800

    setting up

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import pandas as pd
 import pdb
 
 if __name__ == '__main__' :
-    df = pd.read_csv("C:\Users\Ronak Shah\Downloads\quora_duplicate_questions.csv")
+    df = pd.read_csv("quora_duplicate_questions.csv")
+    df = df.ix[0:100, ]
     similarity_score = []
     for index, row in df.iterrows() :
-        try :
-            print(index)
-            similarity_score.append(combine_main(row['question1'], row['question2']))
-        except :
-            similarity_score.append(0)
+#        try :
+        print(index)
+        similarity_score.append(combine_main(row['question1'], row['question2']))
+#        except :
+#            similarity_score.append(0)
+            
+    df['similarity_score'] = similarity_score
+    df.to_csv('Answers.csv')
 
 
