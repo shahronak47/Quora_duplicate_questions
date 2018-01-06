5740754639dcc60085a2594ce74aead18218431d
Ronak Shah <shahronak47@yahoo.in>
2018-01-06 15:33:08 +0800

    changed output

--- similarity_inference.R
+++ similarity_inference.R
@@ -1,10 +1,12 @@
 df$answer <- ifelse(df$similarity_score > 0.5, 1, 0)
 table(df$answer == df$is_duplicate)/nrow(df)
 
+#FALSE      TRUE 
+#0.3465347 0.6534653 
+
 #Get wrong solutions
 new_df <- df[df$answer != df$is_duplicate, ]
 write.csv(new_df, "/Users/ronakshah/Downloads/wrong_answer_2_sample.csv")
-#FALSE      TRUE 
-#0.3465347 0.6534653 
+
 
 
