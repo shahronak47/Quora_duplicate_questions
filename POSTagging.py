22feb9a0c91a6f50d8f13dcd26a53bef01fe6019
Ronak Shah <shahronak47@yahoo.in>
2018-01-21 12:19:51 +0800

    get subjects and objects of close worded questions

--- POSTagging.py
+++ POSTagging.py
@@ -1,6 +1,6 @@
 import re
 import spacy
-import pdb
+import pandas as pd
 
 def get_subject_object (sentence) : 
     #Load the english language
@@ -9,18 +9,24 @@ def get_subject_object (sentence) :
     subject = []
     object = []
     for tok in doc :
-        print(tok.lemma_, tok.text, tok.dep_, tok.tag_)
+        #print(tok.lemma_, tok.text, tok.dep_, tok.tag_)
         #Search for token having subj in them
-        #if re.search('subj', tok.dep_) :
+        if re.search('subj', tok.dep_) :
             #Get the corresponding words
-        #    subject.append(tok.lemma_)
+            subject.append(tok.lemma_)
         # Search for token having obj in them
-        #if re.search('obj', tok.dep_) :
-        #    object.append(tok.lemma_)
+        if re.search('obj', tok.dep_) :
+            object.append(tok.lemma_)
 
     return subject, object
 
-if __name__ == '__main__' : 
-    T1 = "Where can I find a European family office database?"
-    T2 = "Can I make 50,000 a month by day trading?"
-    get_subject_object(T2)
+if __name__ == '__main__' :
+    df = pd.read_csv('close_questions.csv')
+    df['subject1'] = ""
+    df['subject2'] = ""
+    df['object1'] = ""
+    df['object2'] = ""
+    for index, row in df.iterrows():
+        print(index)
+        df.ix[index]['subject1'], df.ix[index]['object1'] = get_subject_object(row['question1'])
+        df.ix[index]['subject2'], df.ix[index]['object2'] = get_subject_object(row['question2'])
